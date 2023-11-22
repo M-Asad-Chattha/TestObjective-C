2e35cb1ee7dbbba3d810d3c0288cf0b540f396a1
Muhammad Asad <muhammad.asad@o9tech.com>
2023-11-22 11:01:26 +0500

    Initial Commit

---
+++ TestObjective-C/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestObjective-C
+//
+//  Created by o9tech on 22/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestObjective-C/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestObjective-C
+//
+//  Created by o9tech on 22/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
